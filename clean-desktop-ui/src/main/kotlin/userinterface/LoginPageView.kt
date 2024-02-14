a6cc5d5fccbccb01131d75a26a0c8e802721098e
Lynn Li <j2557li@uwaterloo.ca>
2024-02-14 13:29:40 -0500

    Added leading and trailing icon for text fields for LoginPage

--- clean-desktop-ui/src/main/kotlin/userinterface/LoginPageView.kt
+++ clean-desktop-ui/src/main/kotlin/userinterface/LoginPageView.kt
@@ -2,16 +2,15 @@ package userinterface
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.*
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -33,25 +32,46 @@ fun LoginPageView(loginPageViewModel: LoginPageViewModel, loginPageController: L
         Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
             Column(verticalArrangement = Arrangement.SpaceEvenly, horizontalAlignment = Alignment.CenterHorizontally) {
                 Image(
-                    painter = painterResource("SignIn.jpg"),
+                    painter = painterResource("SignIn.png"),
                     contentDescription = "SignIn Image",
-                    modifier = Modifier.size(150.dp)
-                        .padding(0.dp,0.dp, 0.dp,0.dp),
+                    modifier = Modifier.size(150.dp),
                     contentScale = ContentScale.Fit)
-
-                //Spacer(modifier = Modifier.height(25.dp))
 
                 TextField(
                     viewModel.email.value,
                     label = {Text("E-mail: ")},
-                    onValueChange = { controller.invoke(LoginPageViewEvent.EmailEvent, it) })
+                    onValueChange = { controller.invoke(LoginPageViewEvent.EmailEvent, it) },
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource("EmailIcon.png"),
+                            contentDescription = "Email",
+                            tint = Color.Gray,
+                            modifier = Modifier.size(30.dp)
+                        )
+                    })
 
                 Spacer(modifier = Modifier.height(25.dp))
 
                 TextField(
                     viewModel.password.value,
                     label = {Text("Password: ")},
-                    onValueChange = { controller.invoke(LoginPageViewEvent.PasswordEvent, it) })
+                    onValueChange = { controller.invoke(LoginPageViewEvent.PasswordEvent, it) },
+                    leadingIcon = {
+                        Icon(
+                            painter = painterResource("PassWordIcon.png"),
+                            contentDescription = "Password",
+                            tint = Color.Gray,
+                            modifier = Modifier.size(30.dp)
+                        )
+                    },
+                    trailingIcon = {
+                        Icon(
+                            painter = painterResource("ViewIcon.png"),
+                            contentDescription = "View",
+                            tint = Color.Gray,
+                            modifier = Modifier.size(30.dp)
+                        )
+                    })
 
                 Spacer(modifier = Modifier.height(25.dp))
 
@@ -62,7 +82,9 @@ fun LoginPageView(loginPageViewModel: LoginPageViewModel, loginPageController: L
                 Spacer(modifier = Modifier.height(25.dp))
 
                 Text("Don't have an account?")
-                Text("Sign Up")
+                TextButton( onClick = {} ) {
+                    Text("Sign Up")
+                }
             }
         }
 
