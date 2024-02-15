0558bd92c6f650f7f6d093d2601f574b7d14976e
Yingjia Zhang <y3457zha@uwaterloo.ca>
2024-02-14 21:55:48 -0500

    Added UI for pages: Add Intake and Exercise

---
+++ CalorieWise/src/main/kotlin/controller/AddExerciseController.kt
@@ -0,0 +1,27 @@
+package controller
+
+import model.UserModel
+import userinterface.AddExerciseEvent
+import userinterface.HomepageComponent
+
+class AddExerciseController(val model: UserModel) {
+    // we can cast `Any` later since each event has an associated type
+    fun invoke(event: AddExerciseEvent, value: Any?) {
+        when(event) {
+            AddExerciseEvent.FirstNameEvent -> model.firstname = value as String
+            AddExerciseEvent.LastNameEvent -> model.lastname = value as String
+            AddExerciseEvent.UppercaseEvent -> {
+                model.firstname = model.firstname.uppercase()
+                model.lastname = model.lastname.uppercase()
+            }
+            AddExerciseEvent.LowercaseEvent -> {
+                model.firstname = model.firstname.lowercase()
+                model.lastname = model.lastname.lowercase()
+            }
+            AddExerciseEvent.ResetEvent -> {
+                model.firstname = ""
+                model.lastname = ""
+            }
+        }
+    }
+}
