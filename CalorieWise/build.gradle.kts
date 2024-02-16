b9af2e4f9d142b741d69dea41010b81cb144228a
Lynn Li <j2557li@uwaterloo.ca>
2024-02-16 14:25:41 -0500

    Changed package name to CalorieWise and added dmg

--- CalorieWise/build.gradle.kts
+++ CalorieWise/build.gradle.kts
@@ -29,7 +29,7 @@ compose.desktop {
 
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
-            packageName = "clean-desktop-ui"
+            packageName = "CalorieWise"
             packageVersion = "1.0.0"
         }
     }
