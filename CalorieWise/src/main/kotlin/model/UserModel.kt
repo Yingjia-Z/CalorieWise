72b1aeb495678a3afa6b6fdb65f0ce121cc8c45b
Peter Li <j2457li@uwaterloo.ca>
2024-03-03 17:00:14 -0500

    [Peter] Implemented authentication functionality, next step is associating user information with their personal caloriewise information

--- CalorieWise/src/main/kotlin/model/UserModel.kt
+++ CalorieWise/src/main/kotlin/model/UserModel.kt
@@ -1,14 +1,11 @@
 package model
 
 enum class Screen {
-    LoginPage,
-    HomePage,
-    BasicInfoPage,
-    IntakePage,
-    AddPage
+    LoginPage, HomePage, BasicInfoPage, IntakePage, AddPage
 
     /* TODO: Food Recommendation Page missing */
 }
+
 class UserModel : IPublisher() {
     var firstname: String = ""
         set(value) {
@@ -29,6 +26,12 @@ class UserModel : IPublisher() {
         }
 
     var password: String = ""
+        set(value) {
+            field = value
+            notifySubscribers()
+        }
+
+    var loggedIn: Boolean = false
         set(value) {
             field = value
             notifySubscribers()
