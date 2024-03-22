e6483c062f2067c4f5fc440ab3240b9280362463
Lynn Li <j2557li@uwaterloo.ca>
2024-03-22 14:57:36 -0400

    Added 3.0.0 installer

--- CalorieWise/build.gradle.kts
+++ CalorieWise/build.gradle.kts
@@ -36,7 +36,7 @@ compose.desktop {
             includeAllModules = true
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "CalorieWise"
-            packageVersion = "2.0.0"
+            packageVersion = "3.0.0"
         }
     }
 }
