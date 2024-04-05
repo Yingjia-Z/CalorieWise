43920d71f538e60ae3d19eeb008dfad54219c036
Peter Li <j2457li@uwaterloo.ca>
2024-04-05 11:02:49 -0400

    [Peter] changed to new gcp database

--- CalorieWise/src/main/kotlin/DatabaseManager.kt
+++ CalorieWise/src/main/kotlin/DatabaseManager.kt
@@ -2,10 +2,12 @@ import java.sql.Connection
 import java.sql.DriverManager
 
 class DatabaseManager(
-    hostname: String = "34.31.38.46:3306",
-    schemaName: String = "calorie-wise"
+    hostname: String = "34.72.35.197:3306",
+    schemaName: String = "calorie-wise",
+    username: String = "root",
+    password: String = "jE5u~(7o,C_bi,h8"
 ) {
-    private val jdbcUrl: String = "jdbc:mysql://$hostname/$schemaName?user=root"
+    private val jdbcUrl: String = "jdbc:mysql://$hostname/$schemaName?user=$username&password=$password"
     private val connection: Connection = establishConnection()
 
     // Establish a database connection
